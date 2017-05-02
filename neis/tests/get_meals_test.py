502b3ffb49fcb0ada940709f6590419cc2c0a6b5
hallazzang <hallazzang@gmail.com>
2017-05-02 12:11:05 +0900

    tests added

---
+++ neis/tests/get_meals_test.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+from neis import NeisClient
+
+def test_get_weekly_meals():
+    client = NeisClient(u'서울')
+    school = client.search_school(u'휘문고등학교')[0]
+    meals = school.get_weekly_meals(2015, 10, 10, 2)
+    assert len(meals) == 7
+    assert not meals[0].menus
+    assert len(meals[1].menus) == 6
+    assert meals[1].menus[2].text == u'안동찜닭'
--- neis/tests/search_school_test.py
+++ neis/tests/search_school_test.py
@@ -6,3 +6,8 @@ def test_search_str():
     client = NeisClient('서울')
     assert len(client.search_school('휘문')) == 2
     assert len(client.search_school('휘문고등학교')) == 1
+
+def test_search_unicode():
+    client = NeisClient(u'서울')
+    assert len(client.search_school(u'휘문')) == 2
+    assert len(client.search_school(u'휘문고등학교')) == 1
